e207692f4206a9510e8cc89d0466aa61a241039d
BryanBmmf <bryan.bmmf@gmail.com>
2019-08-09 13:02:47 -0600

    Actualizando Clase ProcessInstruction

--- src/com/jgranados/koky/instructions/graphicinstructions/ProcessInstruction.java
+++ src/com/jgranados/koky/instructions/graphicinstructions/ProcessInstruction.java
@@ -3,10 +3,19 @@
  */
 package com.jgranados.koky.instructions.graphicinstructions;
 
+import com.jgranados.koky.ui.KokyPointer;
+import java.awt.Graphics2D;
+
 /**
  *
  * @author bryan
  */
-public class ProcessInstruction {
+public class ProcessInstruction extends GraphicsInstruction{
+
+    @Override
+    public Graphics2D execute(Graphics2D graphicsNoPointer, KokyPointer currentPointer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
     
 }
