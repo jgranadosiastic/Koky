858039332f6cb5657446c00a90a60cfd0c14ad37
BryanBmmf <bryan.bmmf@gmail.com>
2019-08-09 12:39:29 -0600

    Creacion de Clase para manejar la instruccion procedimiento

---
+++ src/com/jgranados/koky/instructions/graphicinstructions/ProcessInstruction.java
@@ -0,0 +1,12 @@
+/*
+ * Instruccion para manejar el procedimiento
+ */
+package com.jgranados.koky.instructions.graphicinstructions;
+
+/**
+ *
+ * @author bryan
+ */
+public class ProcessInstruction {
+    
+}
